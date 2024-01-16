8df81f0516d58b61cbed02e36d7dc06c5828374e
Dominic Bird <dominic@dominicbird.com>
2024-01-16 20:13:42 +0000

    Typo in comment

--- teac-dab-controls/install.sh
+++ teac-dab-controls/install.sh
@@ -14,7 +14,7 @@ pip3 install git+https://github.com/domb84/rpi-lcd-menu.git
 # use pwm mode for pigpiod
 sed -i "/ExecStart=/c\ExecStart=/usr/bin/pigpiod -t 0" /lib/systemd/system/pigpiod.service
 
-# fix issue with 3.569 breaking pio permisions
+# fix issue with 3.569 breaking gpio permisions
 # https://community.volumio.com/t/update-to-volumio-3-569-breaks-gpio-permission/64095
 sed -i "s/bcm2835-gpiomem/gpiomem/g" /etc/udev/rules.d/99-com.rules
 
