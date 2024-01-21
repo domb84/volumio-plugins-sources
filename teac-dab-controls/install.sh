f705992abd551479a6e6c8d0b08f166e5f23e2e5
Dominic Bird <dominic@dominicbird.com>
2024-01-21 09:11:27 +0000

    - add ir config - add simple api - add install script to resolve ir conflicts in userconfig.txt

--- teac-dab-controls/install.sh
+++ teac-dab-controls/install.sh
@@ -18,6 +18,16 @@ sed -i "/ExecStart=/c\ExecStart=/usr/bin/pigpiod -t 0" /lib/systemd/system/pigpi
 # https://community.volumio.com/t/update-to-volumio-3-569-breaks-gpio-permission/64095
 sed -i "s/bcm2835-gpiomem/gpiomem/g" /etc/udev/rules.d/99-com.rules
 
+# add ir pins to userconfig
+config_file="/boot/userconfig.txt"
+pattern="dtoverlay=gpio-ir"
+
+# Remove lines containing the specified pattern
+sudo sed -i "/$pattern/d" "$config_file"
+# Add new lines to the end of the file
+echo "dtoverlay=gpio-ir,gpio_pin=25" | sudo tee -a "$config_file" > /dev/null
+echo "dtoverlay=gpio-ir,gpio_pin=6" | sudo tee -a "$config_file" > /dev/null
+
 cp /data/plugins/user_interface/teac-dab-controls/teac-dab-controls.service /lib/systemd/system/
 
 systemctl daemon-reload -q
