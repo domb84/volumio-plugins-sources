f1334fa9b77ba337398d7f0c4fdf0b6a7e361243
Dominic Bird <dominic@dominicbird.com>
2024-01-28 18:02:24 +0000

    - clean up code - clear display on exit

--- teac-dab-controls/uninstall.sh
+++ teac-dab-controls/uninstall.sh
@@ -7,7 +7,7 @@ echo "Uninstalling teac-dab-controls Dependencies"
 # Remove the required packages via apt-get
 apt-get remove -y pigpio python3-dev
 
-# Remove pyhton modules
+# Remove python modules
 pip3 uninstall -y python-engineio==3.14.2 python-socketio[client]==4.6.0 adafruit-blinka Adafruit-PlatformDetect adafruit-python-shell adafruit_circuitpython_mcp3xxx adafruit_circuitpython_bitbangio RPi.GPIO pigpio retrying RpiLCDMenu
 
 # Remove service and reload daemons
